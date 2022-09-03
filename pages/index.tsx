174cb77f5f7cd5ef995c1922369a1ccc5f67d03e
ReneP8 <Rene-Baving@web.de>
2022-09-03 16:47:08 +0200

    Fix: tailwind css not working

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="container">
-      <h1 className="text-3xl font-bold text-red-50">01:59:59</h1>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <p className="text-4xl font-bold text-blue-700">Text</p>
+      <h1 className="text-3xl font-bold text-red-500 uppercase">01:59:59</h1>
     </div>
   );
 };

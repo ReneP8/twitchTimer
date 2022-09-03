a24d538c3aed7eeb87f68685e8ca81e13cac7d79
ReneP8 <Rene-Baving@web.de>
2022-09-03 02:13:01 +0200

    init typescript support and added tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="container">
+      <h1 className="text-3xl font-bold text-red-50">01:59:59</h1>
+    </div>
+  );
+};
+
+export default Home;
